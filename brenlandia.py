6ef2a5b8112be490c6f74124f7f8024951d83b1b
bpalazz <bpalazz@clemson.edu>
2024-04-22 21:10:14 -0400

    yes

--- brenlandia.py
+++ brenlandia.py
@@ -15,31 +15,42 @@ from planets import ExitNotFoundError # imports the ExitNotFoundError class from
 from planets import Planet # imports the Planet class from planets
 from alien import Aliens # imports the Alien function from alien
 
-def handle_riddle(current_planet, riddles, num): # function that handles the riddles from the riddles.csv
+class PlayRiddle: # adds the riddle to the game and inherits from the RiddleReader class in the read_riddles module
 
-    if current_planet.get_name() not in ['Brenlandia', 'Earth']: # if the current planet is not the starting planet or last planet
-        print('You must now answer this riddle so we can keep travelling!')
-        attempts = 2 # attempts per riddle
+    def __init__(self, current_planet, file_path): # initializes the parameters of the class
+        self.reader = RiddleReader(file_path)
+        self.current_planet = current_planet
+        self.riddle = self.reader.read_riddles_from_csv()
 
-        while num < len(riddles['riddle']): # runs until the number of riddles is equal to the length of riddles available
-            print(f'You have {attempts} attempts to answer the riddle')
-            print(f"Find the common word to describe these words: {riddles['riddle'][num]}")
-            answer = input("Your answer: ").strip().lower()
-            
-            if answer == riddles['answer'][num]: # if they get the riddle correct
-                print("Correct!\n")
-                num += 1 # takes the user to the next riddle at the next planet
-                break
+    def handle_riddle(self, num):  # function that handles the riddles from the RiddleReader class
+        if self.current_planet.get_name() not in ['Brenlandia', 'Earth']:  # if the current planet is not the starting planet or last planet
+            print('You must now answer this riddle so we can keep traveling!')
+            attempts = 2  # attempts per riddle
+
+            while num < len(self.riddle['riddle']):  # runs until the number of riddles is equal to the length of riddles available
                 
-            elif attempts == 1: # if they used their attempts this will print
-                print('\nAh! You must be an Alien!! Abort mission! Game Over.')
-                exit(1)
+                print(f'You have {attempts} attempts to answer the riddle.')
+                
+                # Print the riddle
+                print(f"Find the common word to describe these words: {self.riddle['riddle'][num]}")
+                
+                # Get user's answer
+                answer = input("Your answer: ").strip().lower()
 
-            else: # if they get it wrong the first time the hint will print out
-                print("\nIncorrect. Here's a hint: ", riddles['hint'][num])
-                attempts -= 1 # loose an attempt
-        
-    return num # returns the riddle they are on
+                # Check if the answer is correct
+                if answer == self.riddle['answer'][num]:  # if they get the riddle correct
+                    print("Correct!\n")
+                    num += 1  # move to the next riddle
+                    break
+                elif attempts == 1:  # if they used their attempts, this will print
+                    print('\nAh! You must be an Alien!! Abort mission! Game Over.')
+                    exit(1)
+                else:  # if they get it wrong the first time, the hint will print out
+                    print("\nIncorrect. Here's a hint: ", self.riddle['hint'][num])
+                    attempts -= 1  # lose an attempt
+
+        return num  # returns the current riddle index
+
 
 def main():
     num = 0 # starts the riddle function on the first riddle
@@ -56,9 +67,9 @@ def main():
 
     reader = RiddleReader(input_file) # assings reader as the input_file in the RiddleReader class
 
-    riddles = reader() # calls riddles the reader function
+    riddles = reader.read_riddles_from_csv() # calls riddles the reader function
 
-    adventure_map = AdventureMap() # lines 61 - 69 creates the map of the planets
+    adventure_map = AdventureMap() # lines 71 - 79 creates the map of the planets
     adventure_map.add_planet(Planet("Brenlandia", [None], [None]))
     adventure_map.add_planet(Planet("Earth", "I'm sure I don't need to explain what Earth is to you. Anyways get us out of here! And quick!", ["Outer Space"]))
     adventure_map.add_planet(Planet("Jadonia", "This beautiful blue planet puts Neptune and Uranus to shame! Only issue is there is no surface to land on so be careful!", ["Richardian"]))
@@ -73,6 +84,8 @@ def main():
     print('\nWelcome to the Game: "Escape to Brenlandia"\n\nHello space explorer! You have been an outstanding explorer for our society, but now our society is running out of time.\nEarth is dying and we need you to navigate our society to the new planet, Brenlandia.\nThank you for taking this opportunity.\nIf the pressure becomes to much for you to handle you can type "exit" (you\'ll just end humanity!).\nLet\'s get started:') 
     
     current_planet = adventure_map.get_planet("Earth") # initializes the game to start on Earth
+
+    play_riddle = PlayRiddle(current_planet, input_file)
 
     print(current_planet) # prints all the information about Earth
     
@@ -95,6 +108,8 @@ def main():
 
             current_planet = adventure_map.get_planet(planet_choice) # updates the current_planet to the inputted planet
 
+            play_riddle = PlayRiddle(current_planet, input_file)
+
             planets_to_go -= 1 # subtracts 1 from the planets left to go
 
             if current_planet.get_name() == "Brenlandia": # this prints if the user gets to the final planet
@@ -107,12 +122,12 @@ def main():
                 print()
 
             elif planets_to_go == 1: # prints 1 stop away with correct grammar
-                print(f"You are {planets_to_go} stop away from Brenlandia\n")
+                print(f"You are {planets_to_go} planets away from Brenlandia\n")
 
             else: # prints multiple stops away with correct grammar
-                print(f"You are {planets_to_go} stops away from Brenlandia\n")
+                print(f"You are {planets_to_go} planets away from Brenlandia\n")
 
-            num = handle_riddle(current_planet, riddles, num) # raises the riddle for the user to play
+            num = play_riddle.handle_riddle(num) # raises the riddle for the user to play
             
         except ExitNotFoundError as e: # the error statement that will restart the while loop if not actual planet name
             print(f"{e}")
